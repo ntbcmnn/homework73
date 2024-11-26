b3cd6d6cda5a8498e064d9ccc004a942cf889c93
Natalya Botsman <botsmannatashaa@gmail.com>
2024-11-26 22:51:13 +0600

    Created a default message when the main page requested, so instead of 'Cannot get /', the user will see 'Home page'

---
+++ 01/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+    return res.status(200).send(`<h1>Home page</h1>`);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
