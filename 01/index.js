002311df41ad7a369c66e433cb8037a66a7ae3f2
Natalya Botsman <botsmannatashaa@gmail.com>
2024-11-26 22:59:28 +0600

    Added a request to a page that displays text written by user in address bar. Task 1 done

--- 01/index.js
+++ 01/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     return res.status(200).send(`<h1>Home page</h1>`);
 });
 
+app.get('/:echo', (req, res) => {
+    return res.send(`<h1>${req.params.echo}</h1>`);
+});
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
